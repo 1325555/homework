570465a4c7555172b8155c26cba3806489676195
1325555 <a0905915571@gmail.com>
2025-04-25 00:12:14 +0800

    Delete graphic_wave_controller.ino.ino

--- graphic_wave_controller.ino.ino
+++
@@ -1,9 +0,0 @@
-void setup() {
-  Serial.begin(9600); // 設置串口速率
-}
-
-void loop() {
-  int sensorValue = analogRead(A0); // 讀取 A0 引腳的值（可調式電阻）
-  Serial.println(sensorValue);  // 傳送讀取值到串口
-  delay(100); // 延遲，避免數據過於頻繁
-}
